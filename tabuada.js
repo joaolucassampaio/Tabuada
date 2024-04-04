e5cc2d11a4aa10cce780a946b69ce62adf508a6f
João Lucas Sampaio <160886470+joaolucassampaio@users.noreply.github.com>
2024-04-04 15:52:15 -0300

    Update tabuada.js

--- tabuada.js
+++ tabuada.js
@@ -1,3 +1,5 @@
+//Escreva um programa em javascript que seja capaz de calcular a tabuada de um determinado número (de 1 a 20). O programa deve começar pedindo o número a ser usado nos cálculos e então salvar todos os resultados de multiplicações de 1 a 20 usando o número informado dentro de uma variável do tipo string. Depois o programa deve exibir esses resultados e finalizar.
+
 let number = parseFloat(prompt("Digite um número para calcular a tabuada:"))
 let multiplicationTable = ""
 
@@ -7,12 +9,12 @@ for(let i = 1; i <=20; i++) {
 
 alert(multiplicationTable)
 
-// 1 -> Criei uma variável let(permite o valor ser alterado) para armazenar o número do usuário(através do prompt), usando o parseFloat para converter de string para float(o prompt armazena os dados em strings).
+// 3 -> Criei uma variável let(permite o valor ser alterado) para armazenar o número do usuário(através do prompt), usando o parseFloat para converter de string para float(o prompt armazena os dados em strings).
 
-// 2 -> Criei uma variável let vazia.
+// 4 -> Criei uma variável let vazia.
 
-// 4 -> Usei o for para criar uma repetição controlada(para repetir até um valor que eu gostaria). Entre parênteses, eu criei uma variável i(índice), atribuí a ela o valor 1 e passei a condição de que enquanto i <= 20, acontecesse a repetição do for. No final, seria somado +1 no i.
+// 6 -> Usei o for para criar uma repetição controlada(para repetir até um valor que eu gostaria). Entre parênteses, eu criei uma variável i(índice), atribuí a ela o valor 1 e passei a condição de que enquanto i <= 20, acontecesse a repetição do for. No final, seria somado +1 no i.
 
-// 5 -> Misturei variáveis com strings para criar um esqueleto de tabuada.
+// 7 -> Misturei variáveis com strings para criar um esqueleto de tabuada.
 
-// 8 -> Usei a função alert para mostrar os resultados ao usuário.
+// 10 -> Usei a função alert para mostrar os resultados ao usuário.
